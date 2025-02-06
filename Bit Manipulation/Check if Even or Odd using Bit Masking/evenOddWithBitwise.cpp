c998fd775fe1091519420da0b6e54f60829f903d
ZUBAIR PATHAN <160411503+pathanzubair@users.noreply.github.com>
2025-02-06 23:30:26 +0530

    Add files via upload

---
+++ Bit Manipulation/Check if Even or Odd using Bit Masking/evenOddWithBitwise.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <stdint.h>
+
+using namespace std;
+int main(){
+
+    cout << " ******************************** Even Odd Problem using Bitwise operator ******************************** " << endl;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    if(!(num & 1)){
+        cout << "Even" << endl;
+    }
+    else{
+        cout << "Odd" << endl;
+    }
+
+    return 0;
+}
