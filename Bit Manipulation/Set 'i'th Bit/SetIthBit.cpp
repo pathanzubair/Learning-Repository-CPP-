5e130c0ac268b433244ddd1d1d6f8cf8b4cbd635
ZUBAIR PATHAN <160411503+pathanzubair@users.noreply.github.com>
2025-02-07 22:26:50 +0530

    Add files via upload

---
+++ Bit Manipulation/Set 'i'th Bit/SetIthBit.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <stdint.h>
+
+using namespace std;
+
+int setIthBit(int num, int pos){
+    int mask = (1 << pos);
+    return (num | mask);
+}
+
+int main(){
+
+    cout << "************************************************* Set 'i'th Bit *************************************************"<< endl;
+
+    int num;
+    int pos;
+    
+    cout << "Enter the number:";
+    cin >> num;
+    
+
+    cout << "Enter the position:";
+    cin >> pos;
+
+    cout << "The new number = " << setIthBit(num, pos) << endl;
+
+
+
+    return 0;
+}
