d4009fc517d51cc3bb5036f1938c372bdea7084e
ZUBAIR PATHAN <160411503+pathanzubair@users.noreply.github.com>
2025-02-09 19:44:00 +0530

    Add files via upload

---
+++ Bit Manipulation/CLear a Range of Bits/clearBitsIntTheRange.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <stdint.h>
+
+using namespace std;
+
+
+void clearARangeOfBits(int num, int i, int j){
+    int a = (~0) << (j+1);
+    int b = 1 << (i-1);
+
+    int bitmask = a | b;
+    num = num & bitmask;
+
+
+    cout << "The number after clearing bits = "  << num << endl;
+}
+int main(){
+
+    cout << " **************************************************** Clear a Range of Bits ****************************************************" << endl;
+
+    int num, i, j;
+
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Enter i, j: ";
+    cin >> i >> j;
+    cout << endl;
+    clearARangeOfBits(num, i, j);
+
+
+
+
+    return 0;
+}
